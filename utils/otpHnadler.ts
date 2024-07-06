b911814e052cfc808a69ab761dd09af05a767b6b
mrcoder57 <tiwariji2300@gmail.com>
2024-07-06 19:12:45 +0530

    update otphandler

--- utils/otpHnadler.ts
+++ utils/otpHnadler.ts
@@ -3,8 +3,7 @@ import User, { IUser } from "@/models/userModel";
 import { v4 as uuidv4 } from "uuid";
 
 export const transporter = nodemailer.createTransport({
-  host: "sandbox.smtp.mailtrap.io",
-  port: 2525,
+  service: "gmail", // or other services like 'yahoo', 'hotmail
   auth: {
     user: process.env.user,
     pass: process.env.pass,
@@ -12,30 +11,36 @@ export const transporter = nodemailer.createTransport({
 });
 
 export const sendOtpEmail = async (email: string) => {
-  const otp = Math.floor(100000 + Math.random() * 900000).toString();
-  await transporter.sendMail({
-    from: "gigbnbverifi@gmail.com",
-    to: email,
-    subject: 'Your OTP Code',
-    text: `Your OTP code is ${otp}`,
-  });
-  return otp;
+  try {
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    console.log(email);
+    await transporter.sendMail({
+      from: "gigbnbverifi@gmail.com",
+      to: email,
+      subject: "OTp for validation ",
+      text: `Your OTP code is ${otp}`,
+    });
+    console.log("Email sent: ");
+    return otp;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const verifyOtp = async (email: string, otp: string) => {
   const user: IUser | null = await User.findOne({ email });
-
+  
   if (!user || !user.otp || !user.otpExpiry) {
+  
     throw new Error("Invalid OTP");
   }
 
   if (user.otp !== otp || user.otpExpiry < new Date()) {
+ 
     throw new Error("OTP expired or incorrect");
   }
-
   user.otp = undefined;
   user.otpExpiry = undefined;
   await user.save();
 
-  return user;
 };
