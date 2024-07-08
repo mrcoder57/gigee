2dc25088a0ea37f3f3cfdbb522550a1da2367eb1
mrcoder57 <tiwariji2300@gmail.com>
2024-07-08 20:04:07 +0530

    add verify token function

--- utils/jwtHandler.ts
+++ utils/jwtHandler.ts
@@ -1,13 +1,28 @@
 import jwt from "jsonwebtoken";
-const JWT_SECRET=process.env.jwt_secret!;
-export const generateToken = (user:any) => {
-    // Payload to include in the token. Typically, you include user details like id, email, etc.
-    const payload = {
-        id: user._id,
-        email: user.email
-    };
 
-    // Generate the token with an expiration time (e.g., 1 hour)
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '1h' });
-    return token;
+const JWT_SECRET = process.env.jwt_secret!;
+export const generateToken = (user: any) => {
+ 
+  const payload = {
+    userId: user._id,
+    email: user.email,
+  };
+
+  // Generate the token with an expiration time (e.g., 1 hour)
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return token;
+};
+interface TokenPayload {
+  userId: string;
+  email: string;
+}
+
+export const verifyToken = (token: string) => {
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as TokenPayload;
+    const { userId, email } = decoded;
+    return { valid: true, decoded: { userId, email } };
+  } catch (error) {
+    return { valid: false, error };
+  }
 };
