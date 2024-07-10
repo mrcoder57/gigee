d5e0ea29a42eb30be0c09a65e4cf9cfbbe8b182d
mrcoder57 <tiwariji2300@gmail.com>
2024-07-10 19:37:11 +0530

    created verify Token functionality

---
+++ middleware/auth.ts
@@ -0,0 +1,29 @@
+// middleware/auth.js
+import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
+interface JwtPayload {
+  userId: string;
+  isAdmin: boolean;
+}
+
+export function verifyToken(req: any) {
+  const authHeader = req.headers.get("Authorization");
+  const token = authHeader;
+
+  if (!token) {
+    return NextResponse.json(
+      { message: "Access denied. No token provided." },
+      { status: 401 }
+    );
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.jwt_secret!) as JwtPayload;
+    
+    req.userId = decoded.userId;
+    req.isAdmin = decoded.isAdmin;
+    return null; // No error, proceed
+  } catch (error) {
+    return NextResponse.json({ message: "Invalid token." }, { status: 400 });
+  }
+}
