35dc2de3452f5413003d3e8a78930e930caa975c
mrcoder57 <tiwariji2300@gmail.com>
2024-07-12 20:14:20 +0530

    added test components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import UploadComponent from "@/components/imageUpload/upload";
-
+import { Login } from "@/components/modal/modal1";
 export default function Home() {
   return (
     <div className=" flex justify-center space-y-20 ">
       <UploadComponent></UploadComponent>
+      <Login></Login>
     </div>
   );
 }
