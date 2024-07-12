72d909acf03250075d584ad4a0b27c2f2fc71386
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-07-13 03:04:35 +0530

    label add in serach bar

--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -7,18 +7,24 @@ const Search = () => {
       {/* Corrected border class */}
       <div className="border-[1px] rounded-full  w-full md:w-auto py-2  shadow-sm hover:shadow-md transition cursor-pointer">
         <div className="flex flex-row items-center justify-between">
-          <div className="text-sm font-semibold px-6">Anywhere</div>
+          <div className="sm:hidden lg:block font-semibold text-center px-6">
+            
+          <input 
+        type="text" 
+        placeholder="Anywhere"
+        className="w-full sm:w-1/2 md:w-1/3 lg:w-auto" // Full width on small screens, half width on medium screens, third width on large screens, auto width on extra-large screens
+    />
+            
+          </div>
           <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
-            any week
+            Any week
           </div>
           <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
             <form>
               <label
                 htmlFor="guestName"
                 className="block text-sm font-medium text-gray-700"
-              >
-               
-              </label>
+              ></label>
               <input
                 type="text"
                 id="guestName"
