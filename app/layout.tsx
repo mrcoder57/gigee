2802d331da7ed7eac0b77c0c6b68207972758a0e
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-07-12 15:39:53 +0530

    layout change

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/navbar";
+import { Model } from "mongoose";
+import Nodel from "@/components/nodals/nodel";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Nodel/>
         <Navbar/>
+        
+        
         {children}</body>
     </html>
   );
