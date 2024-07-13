20ab08900bec37402d4ec7852ba1dea786b9191e
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-07-13 11:21:57 +0530

    give margin to card

--- components/modal/body.tsx
+++ components/modal/body.tsx
@@ -5,7 +5,7 @@ const Body = () => {
   return (
     
     <div className=' p-2'>
-    <div className='grid grid-cols-1 tablet:grid-cols-2 lg:grid-cols-4 desktop:grid-cols-4 gap-4 justify-center ml-5 gap-y-15'>
+    <div className='grid mt-24 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 desktop:grid-cols-4 gap-4 justify-center ml-5 gap-y-15'>
         <MediaCard/>
         <MediaCard/>
         <MediaCard/>
