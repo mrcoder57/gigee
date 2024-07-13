af6671080a8d3f8dc3f47b028357629266007917
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-07-13 11:21:36 +0530

    Add card component as body

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import UploadComponent from "@/components/imageUpload/upload";
 
 import { Login } from "@/components/modal/modal1";
+import Body from "@/components/modal/body";
 export default function Home() {
   return (
     <div className=" flex justify-center space-y-20 ">
       <UploadComponent></UploadComponent>
       <div className="lg:hidden sm:block justify-center">
         <Login />
+        
       </div>
       
     </div>
