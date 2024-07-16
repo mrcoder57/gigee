3d4210bc1a1ba9b3cbcff40f22cac29dd99bf161
mrcoder57 <tiwariji2300@gmail.com>
2024-07-16 14:05:13 +0530

    add sign up model

--- components/dropdown/Dropdown.tsx
+++ components/dropdown/Dropdown.tsx
@@ -9,18 +9,22 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { AiOutlineMenu } from "react-icons/ai";
 import { Login } from "../modal/modal1";
+import { Signup } from "../modal/sign-up";
 const Dropdown = () => {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
-        <AiOutlineMenu size={24} /> {/* Adjust the size as needed */}
+        <AiOutlineMenu size={16} /> {/* Adjust the size as needed */}
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuLabel>
+          <Signup />
+        </DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem>
-          login
-        </DropdownMenuItem>
+        <DropdownMenuLabel>
+          <Login />
+        </DropdownMenuLabel>
+        <DropdownMenuItem>login</DropdownMenuItem>
         <DropdownMenuItem>Billing</DropdownMenuItem>
         <DropdownMenuItem>Team</DropdownMenuItem>
         <DropdownMenuItem>Subscription</DropdownMenuItem>
---
+++ components/modal/sign-up.tsx
@@ -0,0 +1,100 @@
+"use client";
+import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function Signup() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log(email);
+  return (
+    <div className=" hidden lg:block ">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="link">Signup</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Login</DialogTitle>
+            <DialogDescription>
+              Please provide with your login crendentials
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Email
+              </Label>
+              <Input
+                id="name"
+                defaultValue="example@gmail.com"
+                className="col-span-3"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Email
+              </Label>
+              <Input
+                id="name"
+                defaultValue="example@gmail.com"
+                className="col-span-3"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Email
+              </Label>
+              <Input
+                id="name"
+                defaultValue="example@gmail.com"
+                className="col-span-3"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Password
+              </Label>
+              <Input
+                id="username"
+                defaultValue="@peduarte"
+                className="col-span-3"
+                type="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+          </div>
+          <DialogFooter>
+            <Button type="submit">Login</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
