1e318d7da1a4477c9abd0aa10b9eae2caa1884b5
mrcoder57 <tiwariji2300@gmail.com>
2024-07-16 19:10:54 +0530

    change cards props

--- components/modal/body.tsx
+++ components/modal/body.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import MediaCard from '../ui/card'
+import Cards from '../gigCards/Cards';
 
 const Body = () => {
   return (
     
-    <div className=' p-2'>
-    <div className='grid mt-24 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 desktop:grid-cols-4 gap-4 justify-center ml-5 gap-y-15'>
-        <MediaCard/>
-        <MediaCard/>
-        <MediaCard/>
-        <MediaCard/>
-        <MediaCard/>
-        <MediaCard/>
+    <div className=' my-12 p-2'>
+    <div className='grid mt-24 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 desktop:grid-cols-5 gap-6 justify-around ml-5 gap-y-16'>
+       <Cards/>
+       <Cards/>
+       <Cards/>
+       <Cards/>
+       <Cards/>
+       <Cards/>
+       <Cards/>
+       <Cards/>
     </div>
 </div>
   )
