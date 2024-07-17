c00f20b4cdecd58c75d81e56782737abbdf849d2
mrcoder57 <tiwariji2300@gmail.com>
2024-07-17 21:29:43 +0530

    updated body.tsx

--- components/modal/body.tsx
+++ components/modal/body.tsx
@@ -5,7 +5,7 @@ const Body = () => {
   return (
     
     <div className=' my-12 p-2'>
-    <div className='grid mt-24 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 desktop:grid-cols-5 gap-6 justify-around ml-5 gap-y-16'>
+    <div className='grid mt-24 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 desktop:grid-cols-5 gap-10 justify-around ml-5 lg:gap-y-16 gap-y-8'>
        <Cards/>
        <Cards/>
        <Cards/>
