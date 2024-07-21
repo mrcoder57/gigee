a47ef4746fda801d390b2f7292ca43625c66cbae
mrcoder57 <tiwariji2300@gmail.com>
2024-07-21 22:55:51 +0530

    create page for gigs

---
+++ app/pages/create-gig/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Intro from "@/components/CreateGig/intro";
+
+const Creategig = () => {
+  return (
+    <div className="">
+      <Intro />
+    </div>
+  );
+};
+
+export default Creategig;
