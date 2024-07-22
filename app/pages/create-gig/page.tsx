1889602f2b89e40890d1f3f6624cbfb3cc5552f9
mrcoder57 <tiwariji2300@gmail.com>
2024-07-22 20:25:31 +0530

    modified flex box for form

--- app/pages/create-gig/page.tsx
+++ app/pages/create-gig/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Intro from "@/components/CreateGig/intro";
+import MultiStepForm from "@/components/CreateGig/multi-step-form/form";
 
 
 const Creategig = () => {
@@ -7,6 +8,9 @@ const Creategig = () => {
     <div>
     <div className=" flex flex-col ">
       <Intro />
+      <div className=" flex flex-row items-center justify-center">
+      <MultiStepForm/>
+      </div>
     </div>
     </div>
   );
