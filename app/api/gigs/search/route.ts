feb477a45e3697236995866011048fdcf1030e71
mrcoder57 <tiwariji2300@gmail.com>
2024-07-26 18:36:34 +0530

    create search gig api

---
+++ app/api/gigs/search/route.ts
@@ -0,0 +1,37 @@
+import connectToDb from "@/dbConfig/dbCon";
+import Gig from "@/models/gigMOdel";
+import { NextResponse } from "next/server";
+
+export async function POST(req: any) {
+  await connectToDb();
+
+  try {
+    const { query } = req.query;
+
+    let gigs;
+    if (query) {
+      gigs = await Gig.find({
+        $or: [
+          { title: { $regex: query, $options: "i" } },
+          { location: { $regex: query, $options: "i" } },
+        ],
+      }).limit(10);
+    } else {
+      return NextResponse.json(
+        { message: "seacrh query required" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json(
+      { message: "search results", gigs },
+      { status: 400 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "userId is required" },
+      { status: 400 }
+    );
+  }
+}
