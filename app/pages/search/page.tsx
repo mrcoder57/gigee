dc570948a0342ee222bb1f937fab55d2c1875fbf
mrcoder57 <tiwariji2300@gmail.com>
2024-07-27 20:18:23 +0530

    wrapped usesearparams in suspense

--- app/pages/search/page.tsx
+++ app/pages/search/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Searchbody from "@/components/searchPage/search-body";
-import React from "react";
+import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 const SearchPage: React.FC = () => {
@@ -11,7 +11,9 @@ const SearchPage: React.FC = () => {
   return (
     <div className="flex justify-center space-y-20">
       <div className="sm:block justify-center">
+        <Suspense>
         <Searchbody query={query} />
+        </Suspense>
       </div>
     </div>
   );
