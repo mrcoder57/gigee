59e46eb326d84faf1ca51d64d8976fa858e2aa1e
mrcoder57 <tiwariji2300@gmail.com>
2024-07-27 18:41:06 +0530

    chnage skeleton

--- components/skeleton/skeleton.tsx
+++ components/skeleton/skeleton.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export function SkeletonCard() {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
-      <div className="space-y-2">
+    <div className="flex flex-col mt-10 h-auto lg:w-72 md:w-[300px] w-[300px] mx-auto   rounded-lg overflow-hidden">
+      <Skeleton className="relative w-full h-[310px] rounded-xl shadow-lg" />
+      <div className="space-y-2 mt-5">
         <Skeleton className="h-4 w-[250px]" />
         <Skeleton className="h-4 w-[200px]" />
       </div>
