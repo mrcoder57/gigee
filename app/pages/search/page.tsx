9bfd094c4b8e4961c0bbf2cfa0fbb0b98db9d400
mrcoder57 <tiwariji2300@gmail.com>
2024-07-27 20:28:34 +0530

    wrap search in suspense

--- app/pages/search/page.tsx
+++ app/pages/search/page.tsx
@@ -4,19 +4,23 @@ import React, { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 
 const SearchPage: React.FC = () => {
-  const searchParams = useSearchParams();
-  const query = searchParams.get('q')
- console.log(query)
-
   return (
     <div className="flex justify-center space-y-20">
       <div className="sm:block justify-center">
-        <Suspense>
-        <Searchbody query={query} />
+        <Suspense fallback={<div>Loading...</div>}>
+          <SearchComponent />
         </Suspense>
       </div>
     </div>
   );
+};
+
+const SearchComponent: React.FC = () => {
+  const searchParams = useSearchParams();
+  const query = searchParams.get('q');
+  console.log(query);
+
+  return <Searchbody query={query} />;
 };
 
 export default SearchPage;
