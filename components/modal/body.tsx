056be492f0d4df3d59b53b11c89949c460572843
mrcoder57 <tiwariji2300@gmail.com>
2024-07-27 18:48:06 +0530

    added unique key to each card

--- components/modal/body.tsx
+++ components/modal/body.tsx
@@ -5,6 +5,7 @@ import { toast } from "sonner";
 import { getGigs } from "@/utils/api-handler";
 import Skeletons from "./skeletongrp";
 interface Gig {
+  _id:string
   image: string;
   userId: string;
   title: string;
@@ -43,6 +44,7 @@ const Body = () => {
         <div className="grid mt-24 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 desktop:grid-cols-5 gap-10 justify-around lg:ml-5 lg:gap-y-16 gap-y-8">
           {gigs.map((gig) => (
             <Cards
+            key={gig._id}
               image={gig.image}
               userId={gig.userId}
               title={gig.title}
