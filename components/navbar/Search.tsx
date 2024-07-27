8e229c4107ed7968afffbf8e38468194246bd09d
mrcoder57 <tiwariji2300@gmail.com>
2024-07-27 19:57:35 +0530

    create(hooks):for search

--- components/navbar/Search.tsx
+++ components/navbar/Search.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { BiSearch } from "react-icons/bi";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Search = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const router = useRouter();
+
+  const handleSearch = () => {
+    if (searchQuery.trim()) {
+    
+      router.push(`/pages/search?q=${encodeURIComponent(searchQuery.trim())}`);
+    }
+  };
   return (
     <div>
       <div className="border-[1px] rounded-full  w-full md:w-auto py-2  shadow-sm hover:shadow-md transition cursor-pointer">
         <div className="flex flex-row items-center justify-between">
           <div className="sm:block hidden font-semibold text-center px-6">
-           baad m sochenge
+            baad m sochenge
           </div>
 
           <div className="hidden sm:block text-sm font-semibold px-6 border-x-[1px] flex-1 text-center">
@@ -17,19 +28,24 @@ const Search = () => {
           <div className="text-sm pl-6 pr-2 text-gray-600 flex flex-row items-center gap-3">
             <form>
               <label
-                htmlFor="guestName"
+                htmlFor="search"
                 className="block text-sm font-medium text-gray-700"
               ></label>
               <input
                 type="text"
-                id="guestName"
-                name="guestName"
+                id="search"
                 className="mt-1 block w-full py-2 px-3  bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                placeholder="Add guest:"
+                placeholder="search "
+                value={searchQuery}
+                onChange={(e) => {
+                  setSearchQuery(e.target.value);
+                }}
               />
             </form>
-            <div className="p-2 bg-rose-500 rounded-full text-white">
-              <BiSearch size={18} />
+            <div className=" bg-rose-500 rounded-full text-white">
+              <button className=" p-2 h-full w-full rounded-full" onClick={handleSearch}>
+                <BiSearch size={18} />
+              </button>
             </div>
           </div>
         </div>
