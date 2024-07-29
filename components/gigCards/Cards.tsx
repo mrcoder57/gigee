6ab31db8c2bcf2127be077119d616752224b2102
mrcoder57 <tiwariji2300@gmail.com>
2024-07-29 19:25:47 +0530

    added link to gigs

--- components/gigCards/Cards.tsx
+++ components/gigCards/Cards.tsx
@@ -8,28 +8,32 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import Image from "next/image";
-interface cardProps{
-  image:string,
-  title:string,
-  userId:string,
-  price:number
+import Link from "next/link";
+interface cardProps {
+  gigId: string;
+  image: string;
+  title: string;
+  userId: string;
+  price: number;
 }
-const Cards:React.FC<cardProps> = ({image,title,price,userId}) => {
+const Cards: React.FC<cardProps> = ({ gigId, image, title, price, userId }) => {
   return (
     <div>
       <div className=" flex h-auto lg:w-72 md:w-[300px] w-[300px] mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
         <div className="relative w-full h-[310px]">
-        <Image
-          src="/images/sample.jpg"
-          alt={title}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-lg"
-        />
+          <Image
+            src="/images/sample.jpg"
+            alt={title}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-lg"
+          />
         </div>
       </div>
       <CardHeader>
-        <CardTitle>{title}</CardTitle>
+        <Link href={`/pages/gig/${gigId}`}>
+          <CardTitle>{title}</CardTitle>
+        </Link>
         <CardDescription>hosted by {userId}</CardDescription>
       </CardHeader>
       <CardContent>
