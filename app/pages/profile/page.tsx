e7e3a106147a3e0bee8ab20a979fe375d5f54744
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-07-30 19:03:33 +0530

    profile page create

---
+++ app/pages/profile/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ProfileBody from '@/components/profile/profilepic'
+
+const Profile = () => {
+  return (
+    <div>profile\
+        
+        <ProfileBody/>
+    </div>
+  )
+}
+
+export default Profile
