1bf90dd428129b287a1fff43110f32fec12e51a3
mrcoder57 <tiwariji2300@gmail.com>
2024-07-30 20:53:54 +0530

    added bid cards

--- components/hero/content.tsx
+++ components/hero/content.tsx
@@ -1,27 +1,47 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
-interface contentProps{
-  location:string;
-  userId:string;
-  gigId:string;
-  description:string;
+import Bidcards from "./bidsgrp";
+import Cookies from "js-cookie";
+
+interface contentProps {
+  location: string;
+  userId: string;
+  gigId: string;
+  description: string;
 }
-const Content:React.FC<contentProps> = ({location,userId,description}) => {
+
+const Content: React.FC<contentProps> = ({
+  gigId,
+  location,
+  userId,
+  description,
+}) => {
+  const [isOwner, setIsOwner] = useState(false);
+
+  useEffect(() => {
+    const logUser = Cookies.get("userId");
+    if (logUser === userId) {
+      setIsOwner(true);
+    }
+  }, [userId]);
+
   return (
-    <div className=" flex flex-col max-w-[650px] mb-20">
-      {/* location */}
-      <div className=" flex flex-col ">
+    <div className="flex flex-col max-w-[750px] mb-20">
+      {/* Location */}
+      <div className="flex flex-col">
         <div>
-          <p className=" mt-10 text-2xl font-semibold">
-            Los Angeles, California, Usa {location}
+          <p className="mt-10 text-2xl font-semibold">
+            Los Angeles, California, USA {location}
           </p>
         </div>
       </div>
-      {/* user portion */}
-      <Separator orientation="horizontal" className=" mt-8" />
-      <div className=" flex flex-row h-24 gap-4 items-center ">
-        <div className="w-16 h-16 relative items-center justify-center ">
+      
+      {/* User Portion */}
+      <Separator orientation="horizontal" className="mt-8" />
+      <div className="flex flex-row h-24 gap-4 items-center">
+        <div className="w-16 h-16 relative items-center justify-center">
           <Image
             src="/images/sample.jpg"
             alt="VIP with Kevin Hart"
@@ -30,17 +50,18 @@ const Content:React.FC<contentProps> = ({location,userId,description}) => {
             className="rounded-full"
           />
         </div>
-        <div className=" flex flex-col">
-          <p className=" text-xl font-semibold">Hosted by {userId}</p>
-          <p className=" text-gray-600 text-sm">{userId}</p>
+        <div className="flex flex-col">
+          <p className="text-xl font-semibold">Hosted by {userId}</p>
+          <p className="text-gray-600 text-sm">{userId}</p>
         </div>
       </div>
+      
       <div>
         <Separator orientation="horizontal" />
-        <p className=" text-sm mt-8 ">
-          {description}
-        </p>
+        <p className="text-sm mt-8">{description}</p>
       </div>
+      
+      {isOwner && <Bidcards gigId={gigId} />}
     </div>
   );
 };
