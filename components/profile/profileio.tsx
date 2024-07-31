6b3cc55e797f5c08b1bdc6716a9f8a9ada644332
mrcoder57 <tiwariji2300@gmail.com>
2024-07-31 19:19:22 +0530

    update inputs

--- components/profile/profileio.tsx
+++ components/profile/profileio.tsx
@@ -15,6 +15,8 @@ const ProfileForm: React.FC = () => {
   const [phone, setPhone] = useState("");
   const [education, setEducation] = useState("");
   const [city, setCity] = useState("");
+  const [work,setWork]=useState("")
+  const [hobby,setHobby]=useState("")
   const [socials, setSocials] = useState<string[]>([]);
   const [languages, setLanguages] = useState<string[]>([]);
 
@@ -39,7 +41,7 @@ const ProfileForm: React.FC = () => {
   };
 
   return (
-    <form className="flex flex-col mx-auto p-4 col-span-2 bg-white shadow-md rounded-lg space-y-4 lg:space-y-0 lg:gap-4">
+    <form className="flex flex-col mx-auto p-4 col-span-2 bg-white shadow-md md:w-[450px] lg:w-full w-[300px] rounded-lg space-y-4 lg:space-y-0 lg:gap-4">
       <div className=" flex lg:flex-row flex-col  gap-10">
         <div className="col-span-2">
           <label
@@ -78,7 +80,7 @@ const ProfileForm: React.FC = () => {
             htmlFor="education"
             className="block text-sm font-medium text-gray-700"
           >
-            add ur degree
+            Where i went to school?
           </label>
           <Input
             type="text"
@@ -93,7 +95,7 @@ const ProfileForm: React.FC = () => {
             htmlFor="city"
             className="block text-sm font-medium text-gray-700"
           >
-            where u live?
+            Where i live?
           </label>
           <Input
             type="text"
@@ -101,6 +103,38 @@ const ProfileForm: React.FC = () => {
             name="city"
             value={city}
             onChange={(e) => setCity(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className=" flex lg:flex-row flex-col gap-10">
+        <div className="col-span-2">
+          <label
+            htmlFor="education"
+            className="block text-sm font-medium text-gray-700"
+          >
+            I spent most of time on
+          </label>
+          <Input
+            type="text"
+            id="education"
+            name="education"
+            value={hobby}
+            onChange={(e) => setHobby(e.target.value)}
+          />
+        </div>
+        <div className="col-span-2">
+          <label
+            htmlFor="work"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Kind of Work i do
+          </label>
+          <Input
+            type="text"
+            id="work"
+            name="work"
+            value={work}
+            onChange={(e) => setWork(e.target.value)}
           />
         </div>
       </div>
