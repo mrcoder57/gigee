a735c1bd6022ab39db111727079deca75de41da2
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-07-31 13:14:55 +0530

    tailwind new command

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',  // Add this line if you're using the `app` directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
