c839039c25f8d56d6dc0fa4bad6786f725aa4ee2
mrcoder57 <tiwariji2300@gmail.com>
2024-08-10 19:48:44 +0530

    added logout to menus

--- components/dropdown/Dropdown.tsx
+++ components/dropdown/Dropdown.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
 import {
   DropdownMenu,
@@ -13,11 +14,13 @@ import { Login } from "../modal/login";
 import { Signup } from "../modal/sign-up";
 import Cookies from "js-cookie";
 import Link from "next/link";
+import { logOut } from "@/utils/api-handler"; 
+import { useRouter } from "next/navigation";
 
 const Dropdown = () => {
   const [loggedIn, setLoggedIn] = useState(false);
   const [userId, setUserId] = useState("");
-
+  const router = useRouter();
   useEffect(() => {
     const token = Cookies.get("token");
     const userId = Cookies.get("userId");
@@ -27,24 +30,45 @@ const Dropdown = () => {
     }
   }, []);
 
+  const handleLogout = async () => {
+    await logOut();
+    setLoggedIn(false);
+    setUserId("");
+    router.push("/");
+  };
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger className=" border-blue-50 focus:outline-none">
+      <DropdownMenuTrigger className="border-blue-50 focus:outline-none">
         <div className="w-16 h-10 p-3 flex items-center justify-center border-2 rounded-full">
-        <AiOutlineMenu size={16} /> {/* Adjust the size as needed */}
+          <AiOutlineMenu size={16} /> {/* Adjust the size as needed */}
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
         <DropdownMenuSeparator />
-        <DropdownMenuLabel>
-          <Login />
-        </DropdownMenuLabel>
-        {loggedIn && (
-          <DropdownMenuItem className="text-center font-semibold">
-            <Link href={`/pages/profile/${userId}`}>
-              <span className="text-center font-normal ml-2">Edit Profile</span>
-            </Link>
-          </DropdownMenuItem>
+        {loggedIn ? (
+          <>
+            <DropdownMenuItem className="text-center hover:underline hover:text-gray-600 transition-colors duration-200">
+              <Link href={`/pages/profile/${userId}`}>
+                <span className="text-center font-normal ml-2">Edit Profile</span>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              className=" ml-2 cursor-pointer text-center font-medium hover:underline hover:text-gray-600 transition-colors duration-200 "
+              onClick={handleLogout}
+            >
+              Logout
+            </DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuLabel>
+              <Login />
+            </DropdownMenuLabel>
+            <DropdownMenuItem className="text-center font-semibold">
+              {/* <Signup /> Optional: If you have a Signup option */}
+            </DropdownMenuItem>
+          </>
         )}
       </DropdownMenuContent>
     </DropdownMenu>
