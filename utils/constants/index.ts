7aabe1094e1c94af02d424544243de2cf4f2e26b
mrcoder57 <tiwariji2300@gmail.com>
2024-08-12 18:48:11 +0530

    constants for cities

---
+++ utils/constants/index.ts
@@ -0,0 +1,58 @@
+type City = {
+    name: string;
+    state: string;
+  };
+  
+  const citiesInIndia: City[] = [
+    { name: "Mumbai", state: "Maharashtra" },
+    { name: "Delhi", state: "IN" },
+    { name: "Bangalore", state: "Karnataka" },
+    { name: "Hyderabad", state: "Telangana" },
+    { name: "Chennai", state: "Tamil Nadu" },
+    { name: "Kolkata", state: "West Bengal" },
+    { name: "Ahmedabad", state: "Gujarat" },
+    { name: "Pune", state: "Maharashtra" },
+    { name: "Jaipur", state: "Rajasthan" },
+    { name: "Lucknow", state: "Uttar Pradesh" },
+    { name: "Kanpur", state: "Uttar Pradesh" },
+    { name: "Nagpur", state: "Maharashtra" },
+    { name: "Visakhapatnam", state: "Andhra Pradesh" },
+    { name: "Bhopal", state: "Madhya Pradesh" },
+    { name: "Patna", state: "Bihar" },
+    { name: "Vadodara", state: "Gujarat" },
+    { name: "Ludhiana", state: "Punjab" },
+    { name: "Agra", state: "Uttar Pradesh" },
+    { name: "Nashik", state: "Maharashtra" },
+    { name: "Faridabad", state: "Haryana" },
+    { name: "Meerut", state: "Uttar Pradesh" },
+    { name: "Rajkot", state: "Gujarat" },
+    { name: "Kochi", state: "Kerala" },
+    { name: "Coimbatore", state: "Tamil Nadu" },
+    { name: "Mysore", state: "Karnataka" },
+    { name: "Guwahati", state: "Assam" },
+    { name: "Amritsar", state: "Punjab" },
+    { name: "Jodhpur", state: "Rajasthan" },
+    { name: "Indore", state: "Madhya Pradesh" },
+    { name: "Vijayawada", state: "Andhra Pradesh" },
+    { name: "Ranchi", state: "Jharkhand" },
+    { name: "Raipur", state: "Chhattisgarh" },
+    { name: "Thiruvananthapuram", state: "Kerala" },
+    { name: "Jabalpur", state: "Madhya Pradesh" },
+    { name: "Madurai", state: "Tamil Nadu" },
+    { name: "Allahabad", state: "Uttar Pradesh" },
+    { name: "Varanasi", state: "Uttar Pradesh" },
+    { name: "Aurangabad", state: "Maharashtra" },
+    { name: "Gwalior", state: "Madhya Pradesh" },
+    { name: "Noida", state: "Uttar Pradesh" },
+    { name: "Bhubaneswar", state: "Odisha" },
+    { name: "Dehradun", state: "Uttarakhand" },
+    { name: "Shimla", state: "Himachal Pradesh" },
+    { name: "Srinagar", state: "Jammu and Kashmir" },
+    { name: "Panaji", state: "Goa" },
+    { name: "Gangtok", state: "Sikkim" },
+    { name: "Puducherry", state: "Puducherry" },
+    { name: "Shillong", state: "Meghalaya" },
+  ];
+  
+  export default citiesInIndia;
+  
