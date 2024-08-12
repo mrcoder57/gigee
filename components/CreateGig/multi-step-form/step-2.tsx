c822063547bcca9f5a56490f71c916ffb2ad360a
mrcoder57 <tiwariji2300@gmail.com>
2024-08-12 18:49:00 +0530

    add citiy  compoent

--- components/CreateGig/multi-step-form/step-2.tsx
+++ components/CreateGig/multi-step-form/step-2.tsx
@@ -1,5 +1,5 @@
-// components/Step2.tsx
-import React from "react";
+"use client"
+import React,{useState} from "react";
 import {
   CardContent,
   CardDescription,
@@ -9,13 +9,39 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-
+import citiesInIndia from "@/utils/constants";
+import CityList from "@/components/city/city";
 const Step2: React.FC<{
   nextStep: () => void;
   prevStep: () => void;
   handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   values: any;
 }> = ({ nextStep, prevStep, handleChange, values }) => {
+  const [filteredCities, setFilteredCities] = useState(citiesInIndia);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCityFilter = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value.toLowerCase();
+    setFilteredCities(
+      citiesInIndia.filter((city) =>
+        city.name.toLowerCase().includes(searchTerm)
+      )
+    );
+    handleChange(e);
+  };
+
+ 
+  const handleSelectCity = (city: { name: string; state: string }) => {
+    const location = `${city.name}, ${city.state}`;
+    handleChange({
+      target: {
+        name: "location",
+        value: location,
+      },
+    } as React.ChangeEvent<HTMLInputElement>);
+    setFilteredCities(citiesInIndia); 
+    setIsOpen(false);
+  };
   return (
     <div className="flex flex-col h-auto mt-16 mb-16 lg:w-[550px] md:w-64 w-64 mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
       <CardHeader>
@@ -33,9 +59,9 @@ const Step2: React.FC<{
               type="number"
               name="price"
               className="col-span-3"
-              
               value={values.price}
               onChange={handleChange}
+
             />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
@@ -48,9 +74,13 @@ const Step2: React.FC<{
               className="col-span-3"
               type="text"
               value={values.location}
-              onChange={handleChange}
+              onChange={handleCityFilter}
+              onFocus={() => setIsOpen(true)}
+              onBlur={() => setTimeout(() => setIsOpen(false), 100)} 
+              placeholder="Start typing to filter cities..."
             />
           </div>
+          <CityList cities={filteredCities} onSelectCity={handleSelectCity} isOpen={isOpen}  />
         </div>
       </CardContent>
       <div className=" flex flex-row justify-between mx-6 mb-5">
