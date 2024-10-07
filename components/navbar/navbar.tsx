2f0ab0ca46b4ef3a16edb53757e1e13273fe7f11
mrcoder57 <tiwariji2300@gmail.com>
2024-10-07 22:29:01 +0530

    change Navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,39 +1,76 @@
 "use client";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import Container from "../Container";
 import Logo from "./Logo";
 import Search from "./Search";
 import Usermenu from "./Usermenu";
 import Notifications from "../notificationns/notification";
+import Link from "next/link";
+import { NavLinks } from "@/utils/constants";
 
-function navbar() {
+function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const isHomePage = pathName === "/";
+
   return (
-    <div className="fixed flex flex-row items-center justify-between w-full bg-white z-10 shadow-sm overflow-x-hidden h-20 ">
-      <div className="border-b-1px flex flex-row items-center justify-between w-full h-12 ">
-        <Container>
-          <div
-            className="
-                flex
-                flex-row
-                items-center
-                justify-between
-                gap-x-3
-                overflow-x-hidden
-               
-                "
-          >
-            <Logo />
-            <Search />
-           <div className=" flex flex-row items-center gap-x-3 overflow-x-hidden">
-            <Notifications />
-            <Usermenu />
+    <div className={`fixed flex flex-col top-0 left-0 w-full z-50 transition-all duration-300 bg-white`}>
+      {/* Navbar starts */}
+      <div className="flex flex-row items-center justify-between mt-4 mb-4 lg:mx-9 mx-4">
+        <div className="flex items-center justify-center">
+          <Logo />
+        </div>
+        <div className="flex flex-row items-center justify-center gap-3">
+          {isHomePage ? (
+            // Show NavLinks only on the home page
+            (!isScrolled ? NavLinks.map((link, index) => (
+              <div
+                key={index}
+                className="lg:block hidden text-[14px] font-semibold text-center px-6 p-2 hover:bg-gray-100 hover:p-2 hover:px-6 hover:rounded-full"
+              >
+                <Link href={`/pages/${link.Link}`}>{link.name}</Link>
+              </div>
+            )) : (
+              // Show Search when scrolled on home page
+              <div className="hidden sm:flex flex-row items-center">
+                <Search />
+              </div>
+            ))
+          ) : (
+            // Show Search on non-home pages
+            <div className="hidden sm:flex flex-row items-center">
+              <Search />
             </div>
-          </div>
-      
-        </Container>
+          )}
+        </div>
+        <div className="flex flex-row items-center justify-center gap-1">
+          <Notifications />
+          <Usermenu />
+        </div>
       </div>
+      {/* Navbar ends */}
+      {(!isScrolled && isHomePage) && (
+        <div className="lg:flex hidden items-center justify-center mt-0 mb-2">
+          <Search />
+        </div>
+      )}
+      <hr />
     </div>
   );
 }
 
-export default navbar;
+export default Navbar;
