3aca91940b3f7bce9b95f0dc4afb8743e081d99d
mrcoder57 <tiwariji2300@gmail.com>
2024-10-07 22:27:24 +0530

    change width

--- components/Container.tsx
+++ components/Container.tsx
@@ -6,7 +6,7 @@ interface ContainerProps {
 
 const Container: React.FC<ContainerProps> = ({ children }) => {
   return (
-    <div className="max-w-full overflow-x-hidden top-0 w-full lg:mx-12">
+    <div className="max-w-full overflow-x-hidden top-0 w-full lg:mx-12 my-2  rounded-xl">
       {children}
     </div>
   );
