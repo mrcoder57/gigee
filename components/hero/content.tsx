74cf514e03faa8cd68db0788d8ca9051f4799af6
mrcoder57 <tiwariji2300@gmail.com>
2024-10-09 21:48:09 +0530

    update Ui

--- components/hero/content.tsx
+++ components/hero/content.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Separator } from "@/components/ui/separator";
+
 import Image from "next/image";
 import Bidcards from "./bidsgrp";
 import Cookies from "js-cookie";
@@ -52,41 +52,45 @@ const Content: React.FC<ContentProps> = ({
   }, [userId]);
 
   return (
-    <div className="flex flex-col justify-center lg:ml-5 max-w-[750px] mb-20 ">
+    <div className="flex flex-col lg:justify-between max-w-screen lg:w-full lg:mx-16 mx-5  mb-20 ">
       {/* Location */}
-      <div className="flex flex-col justify-center ">
+      <div className="flex flex-col lg:justify-center ">
         <div>
           <p className="mt-10 text-2xl font-semibold">{location}</p>
         </div>
       </div>
 
       {/* User Portion */}
-      <Separator orientation="horizontal" className="mt-8" />
-      <div className="flex flex-row h-24 gap-4 items-center mt-2 mb-1">
-        <div className="w-16 h-16 relative items-center justify-center">
-          {profile && (
-            <Image
-              src={profile.profilePic || "/images/sample.jpg"}
-              alt={`Profile picture of ${creatorName}`}
-              layout="fill"
-              objectFit="cover"
-              className="rounded-full"
-            />
-          )}
+      <div className=" flex flex-col justify-center my-auto max-h-[75px] mt-2 ">
+        <hr className=" lg:w-[80vh] " />
+        <div className="flex flex-row h-24 gap-x-4 items-center  my-auto">
+          <div className="w-12 h-12 relative flex items-center justify-center">
+            {profile && (
+              <Image
+                src={profile.profilePic || "/images/sample.jpg"}
+                alt={`Profile picture of ${creatorName}`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full"
+              />
+            )}
+          </div>
+          <div className="flex flex-col">
+            <Link href={`/pages/profile/${userId}`}>
+              <p className="text-lg font-semibold">Hosted by {creatorName}</p>
+            </Link>
+            <p className="text-gray-600 text-sm">{creatorName}</p>
+          </div>
         </div>
-        <div className="flex flex-col">
-          <Link href={`/pages/profile/${userId}`}>
-            <p className="text-xl font-semibold">Hosted by {creatorName}</p>
-          </Link>
-          <p className="text-gray-600 text-sm">{creatorName}</p>
-        </div>
+        <hr className=" lg:w-[80vh]" />
+        
       </div>
-
       <div>
-        <Separator orientation="horizontal" />
-        <p className="text-sm mt-8">{description}</p>
+          <p className="text-sm mt-8">{description}</p>
+        </div>
+      <div className="flex flex-row items-center mt-5">
+        {isOwner && <Bidcards gigId={gigId} />}
       </div>
-      <div className="mt-5">{isOwner && <Bidcards gigId={gigId} />}</div>
     </div>
   );
 };
