e83a4b0b9f301734273721201b2260522ba368a8
mrcoder57 <tiwariji2300@gmail.com>
2024-10-09 21:48:29 +0530

    fix instance

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,3 +1,9 @@
 import { io } from 'socket.io-client';
 
  export const socketInstance = io();
+
+ socketInstance.on("connect",
+    ()=>{
+        console.log('Socket connected:', socketInstance.connected);
+    }
+ )
