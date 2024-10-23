7e226c7e740cf6026813c3207cc416248d792476
mrcoder57 <tiwariji2300@gmail.com>
2024-10-23 19:13:10 +0530

    fix: margins and Ui

--- components/chats/sendMessage.tsx
+++ components/chats/sendMessage.tsx
@@ -3,10 +3,10 @@ import { IoSendOutline } from "react-icons/io5";
 import { MdAdd } from "react-icons/md";
 const SendMessage = () => {
   return (
-    <div className=' flex items-center bg-gray-200 w-full rounded-full h-16 justify-between mb-2 '>
-    <MdAdd size={26} className=' ml-5'/>
-    <input type="text" className=' bg-gray-200 w-[80%] h-full outline-none focus:outline-none mr-1 ' placeholder='Type ' />
-    <IoSendOutline size={26} className=' mr-5'/>
+    <div className=' flex absolute bottom-[4px] items-center my-auto bg-white w-full h-[50px] justify-between mb-[4px] '>
+    <MdAdd size={26} className=' ml-5 mt-[3px]'/>
+    <input type="text" className=' mt-[2px] bg-gray-100 rounded-md p-2 w-[85%] h-[40px] outline-none focus:outline-none mr-1 ' placeholder='Type ' />
+    <IoSendOutline size={26} className=' mr-5 mt-[3px]'/>
 </div>
   )
 }
