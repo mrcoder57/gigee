528b5195a8c1c6aab7c8edbf1d4b1a05c3f72b3e
mrcoder57 <tiwariji2300@gmail.com>
2024-10-23 19:12:04 +0530

    fix:responsivenes

--- components/chats/chats.tsx
+++ components/chats/chats.tsx
@@ -2,13 +2,17 @@ import React from 'react'
 import UserHeader from './userHeaader'
 import ScrollableChats from './scrollableChats'
 import SendMessage from './sendMessage'
+type chatsProp={
+  recieverName:string | null,
+  status:string
+}
 
-const Chats = () => {
+const Chats:React.FC<chatsProp> = ({recieverName,status}) => {
   return (
     <div className=' lg:flex hidden flex-col h-screen w-full overflow-y-hidden shadow-md  ml-2'>
-        <UserHeader></UserHeader>
+        <UserHeader status='active' recieverName={recieverName}/>
         <ScrollableChats/>
-        <div className=' mx-5'>
+        <div className=' mx-5 relative'>
         <SendMessage/>
         </div>
     </div>
