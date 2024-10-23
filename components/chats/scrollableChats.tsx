3ed147734de30d0ba14434f85f94ac0c5c0c6bb9
mrcoder57 <tiwariji2300@gmail.com>
2024-10-23 19:12:49 +0530

    fix height

--- components/chats/scrollableChats.tsx
+++ components/chats/scrollableChats.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { sampleMessages } from "@/utils/constants";
 
+
 const ScrollableChats = () => {
   return (
-    <div className="flex-grow w-full">
-      <ScrollArea className="w-full h-[600px] rounded-md border p-4 overflow-y-auto fixed top-0">
+    <div className="flex-grow w-full ">
+      <ScrollArea className="w-full h-[640px] px-4 overflow-y-auto fixed top-2">
         {sampleMessages.map((message) => (
           <div key={message.id} className="mb-4 bg-green-100 rounded-lg max-w-80 w-80 h-auto items-center justify-center">
             <p className="text-gray-700 p-2">{message.text}</p>
