de19e872e16837e4d0478cee49528c546e8b6f74
mrcoder57 <tiwariji2300@gmail.com>
2024-11-05 18:41:43 +0530

    add intro map

--- components/CreateGig/intro.tsx
+++ components/CreateGig/intro.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState } from "react";
 import { Slider } from "@/components/ui/slider";
+import Map from "../map/map";
+import IntroMap from "./introMap";
 
 const Intro = () => {
   const [sliderValue, setSliderValue] = useState([400]);
@@ -10,11 +12,12 @@ const Intro = () => {
   };
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="lg:mt-56 mt-32 lg:max-w-[50%] items-center">
-        <h2 className="text-red-400 text-3xl font-semibold text-center">
-          Create ur gig <br />
-          <span className="text-black text-3xl font-semibold">
+    <div className=" flex lg:flex-row flex-col w-full max-w-7xl items-center justify-between mx-auto h-full lg:pt-36 pt-24 "> 
+    <div className="flex flex-col items-center  ">
+      <div className=" lg:max-w-sm items-center">
+        <h2 className="text-red-400 lg:text-[34px] text-[24px] font-bold text-center">
+           Host any Events, Create any Jobs anywhere and many more <br />
+          <span className="text-black  font-bold">
             You get the Job done for
           </span>
         </h2>
@@ -24,7 +27,7 @@ const Intro = () => {
           defaultValue={sliderValue}
           max={10000}
           step={1}
-          className="lg:px-80 px-40 rounded-md bg-black"
+          className="lg:px-60 px-40 mb-5 h-[4px] rounded-md bg-[#6e6e6e6e]"
           onValueChange={(value) => handleSliderChange(value)}
           value={sliderValue}
         />
@@ -32,6 +35,8 @@ const Intro = () => {
           {sliderValue}
           </h2>
       </div>
+    </div>
+   <IntroMap/>
     </div>
   );
 };
