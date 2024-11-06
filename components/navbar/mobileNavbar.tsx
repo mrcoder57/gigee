68941347346b59bf41d90c200073e56a2a40774c
mrcoder57 <tiwariji2300@gmail.com>
2024-11-06 16:35:13 +0530

    feature: add category carasole

--- components/navbar/mobileNavbar.tsx
+++ components/navbar/mobileNavbar.tsx
@@ -6,19 +6,18 @@ import Search from "./Search";
 
 import Usersheet from "./userSidebar/usersheet";
 import BackButton from "./back";
+import CategoriesToggles from "../categories/categoriesCarousel";
 
 function MobileNavbar() {
- 
   const pathName = usePathname();
 
- 
   const isChatsPage = pathName === "/chats";
   if (isChatsPage) return null;
 
   return (
     <div className="flex lg:hidden md:hidden items-center justify-center w-full bg-white  ">
       <div
-        className={` flex flex-col fixed top-0 z-50 w-full justify-center items-center bg-white px-3 h-[70px] lg:h-auto shadow-sm `}
+        className={` flex flex-col fixed top-0 z-50 w-full justify-center items-center bg-white px-3 h-auto lg:h-auto shadow-sm `}
       >
         {/* Navbar starts */}
         <div className="flex flex-row items-center justify-between max-w-7xl py-2 w-full mx-auto">
@@ -26,7 +25,7 @@ function MobileNavbar() {
             <BackButton />
           </div>
           <div className="flex flex-row items-center justify-center gap-3">
-            <Search/>
+            <Search />
           </div>
           <div className="flex flex-row items-center justify-center lg:gap-x-7 gap-x-3 ">
             <Usersheet />
@@ -34,9 +33,7 @@ function MobileNavbar() {
         </div>
         {/* Navbar ends */}
 
-    
-
-       
+        <CategoriesToggles />
       </div>
     </div>
   );
