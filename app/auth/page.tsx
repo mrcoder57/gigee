bf56ab847b1f7e5f5b35e9201a80eacb45060c16
mrcoder57 <tiwariji2300@gmail.com>
2024-11-08 11:28:47 +0530

    feature create new auth page

---
+++ app/auth/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from '@/components/authform/authForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=' py-20  h-screen flex items-center justify-center w-full'>
+        <AuthForm/>
+    </div>
+  )
+}
+
+export default page
