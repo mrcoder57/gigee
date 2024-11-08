0faac5e28727a063447290161614363e195cdd84
mrcoder57 <tiwariji2300@gmail.com>
2024-11-08 11:40:16 +0530

    fix: format

--- components/chats/chats.tsx
+++ components/chats/chats.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import UserHeader from './userHeaader'
-import ScrollableChats from './scrollableChats'
-import SendMessage from './sendMessage'
-type chatsProp={
-  recieverName:string | null,
-  status:string
-}
+import React from "react";
+import UserHeader from "./userHeaader";
+import ScrollableChats from "./scrollableChats";
+import SendMessage from "./sendMessage";
+type chatsProp = {
+  recieverName: string | null;
+  status: string;
+};
 
-const Chats:React.FC<chatsProp> = ({recieverName,status}) => {
+const Chats: React.FC<chatsProp> = ({ recieverName, status }) => {
   return (
-    <div className=' lg:flex hidden flex-col h-screen w-full overflow-y-hidden shadow-md  ml-2'>
-        <UserHeader status='active' recieverName={recieverName}/>
-        <ScrollableChats/>
-        <div className=' mx-5 relative'>
-        <SendMessage/>
-        </div>
+    <div className=" lg:flex hidden flex-col h-screen w-full overflow-y-hidden shadow-md  ml-2">
+      <UserHeader status="active" recieverName={recieverName} />
+      <ScrollableChats />
+      <div className=" mx-5 relative">
+        <SendMessage />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chats
+export default Chats;
