22c10cd4bab5423ba3410e121968df9db2337200
mrcoder57 <tiwariji2300@gmail.com>
2024-11-09 13:47:49 +0530

    wrap inside session provider

--- app/create-gig/page.tsx
+++ app/create-gig/page.tsx
@@ -1,33 +1,42 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import Intro from "@/components/CreateGig/intro";
 import MultiStepForm from "@/components/CreateGig/multi-step-form/form";
-import Cookies from "js-cookie";
-import { Button } from "@/components/ui/button";
-import { Login } from "@/components/modal/login";
 import Anytime from "@/components/CreateGig/landingPage/anytime/anytime";
 import AuthForm from "@/components/authform/authForm";
+import { SessionProvider, useSession } from "next-auth/react";
 
+// The component wrapped inside SessionProvider to access session state
 const Creategig = () => {
+  return (
+    <SessionProvider>
+      <GigPage />
+    </SessionProvider>
+  );
+};
+
+const GigPage = () => {
+  const { data: session, status } = useSession();
   const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    const token = Cookies.get("token");
-    if (token) {
+    if (status === "authenticated") {
       setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
     }
-  }, []);
+  }, [status]);
 
   return (
-    <div className=" h-full w-full flex flex-col ">
+    <div className="h-full w-full flex flex-col">
       <div className="flex flex-col h-full">
         <Intro />
-        <Anytime/>
+        <Anytime />
         <div className="flex flex-row items-center justify-center mt-10 mb-9">
           {loggedIn ? (
             <MultiStepForm />
           ) : (
-           <AuthForm/>
+            <AuthForm />
           )}
         </div>
       </div>
