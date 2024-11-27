b31369a497dcde8286229abf8c542432b54aee1c
mrcoder57 <tiwariji2300@gmail.com>
2024-11-27 21:43:41 +0530

    fix: responsiveness

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 flex items-center justify-center p-4 sm:p-6 lg:p-8">
+    <div className="min-h-screen bg-gradient-to-b py-16 md:py-0 lg:py-0 from-gray-50 to-gray-100 flex items-center justify-center p-4 sm:p-6 lg:p-8">
       <div className="max-w-6xl flex items-center w-full">
-        <div className="flex flex-col md:flex-row-reverse items-center md:items-center justify-between w-full space-y-8 md:space-y-0 md:space-x-8">
-          <div className="w-full md:w-1/2">
+        <div className="flex flex-col md:flex-row-reverse items-center md:items-center justify-between w-full gap-y-8">
+          <div className="w-full md:w-1/2 items-center justify-center flex">
            <Image src={"/not-found.png"} width={500} height={500} alt="404" className=' lg:w-[500px] lg:h-[500px] w-[320px] h-[320px]' />
           </div>
           <div className="w-full md:w-1/2 space-y-6">
@@ -18,7 +18,7 @@ export default function NotFound() {
             </p>
             <div className="space-y-4">
               <p className="text-lg text-gray-600 text-center md:text-left">Possible reasons:</p>
-              <ul className="list-disc list-inside text-gray-600 space-y-2 text-center md:text-left">
+              <ul className="list-disc list-inside text-gray-600 space-y-2 flex items-center flex-col w-full text-center md:text-left">
                 <li>The page may have been moved or deleted</li>
                 <li>You might have typed the wrong URL</li>
               </ul>
