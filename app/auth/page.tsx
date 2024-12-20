f21b15d598d9501637c1150afd6b48fbe24f11c3
Anil yadav <92326265+anilyadav6803@users.noreply.github.com>
2024-12-20 10:13:26 +0530

    Add: import authform

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import { testMessage } from '@/utils/socket'; // Import the socketInstance
 import {io } from "socket.io-client";
+import AuthForm from '@/components/authform/authForm';
 
 const Page = () => {
   // Create a new Socket.IO instance
@@ -11,7 +12,7 @@ const Page = () => {
   return (
     <div className='flex flex-col py-20 h-screen items-center justify-center w-full'>
       <p className=' text-3xl'>{data}</p> {/* This will display the message from the "test" event */}
-      {/* <AuthForm /> */}
+      <AuthForm />
     </div>
   );
 };
